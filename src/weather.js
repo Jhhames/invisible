2053db19f823bce0a4cd91bf8377fec6cea86ff8
James Falola <fjhhames@gmail.com>
2020-06-07 12:52:26 +0100

    Weather Abstraction

---
+++ src/weather.js
@@ -0,0 +1,31 @@
+import api from "./api";
+
+/**
+ * Weather Abstraction
+ * This communicates with the APIs and logs results on the console
+ */
+
+const weather = {
+  getWeatherAndTime(locationArray) {
+    for (let i = 0, length = locationArray.length; i < length; i++) {
+      const location = locationArray[i];
+      api
+        .getWeatherAndTime(location)
+        .then(this.logWeatherAndTime)
+        .catch(e => {
+          console.log(`%c${location} Error: ${e.message} `, "color:red");
+        });
+    }
+  },
+
+  logWeatherAndTime(response) {
+    const { temperature, weather_descriptions } = response.data.current;
+    const { name, localtime } = response.data.location;
+    console.log(
+      `%cLocation: ${name}; Current Time: ${localtime}; Weather: ${weather_descriptions.toString()}; Temperature: ${temperature} degrees C`,
+      "color:green"
+    );
+  }
+};
+
+export default weather;
